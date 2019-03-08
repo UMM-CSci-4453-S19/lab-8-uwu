faca86dfb6ba4d049e9f9fb26d4f3edd64555f4a
songx827 <songx827@morris.umn.edu>
2019-03-08 15:47:39 -0600

    Add .gitignore

--- express.js
+++ express.js
@@ -1,8 +1,8 @@
 var express=require('express'),
-mysql=require('mysql'),
-credentials=require('./credentials.json'),
-app = express(),
-port = process.env.PORT || 1337;
+    mysql=require('mysql'),
+    credentials=require('./credentials.json'),
+    app = express(),
+    port = process.env.PORT || 1337;
 
 credentials.host='ids.morris.umn.edu'; //setup database credentials
 
@@ -12,24 +12,23 @@ connection.connect(function(err){if(err){console.log(error)}});
 
 app.use(express.static(__dirname + '/public'));
 app.get("/buttons",function(req,res){
-  var sql = 'SELECT * FROM test.till_buttons';
-  connection.query(sql,(function(res){return function(err,rows,fields){
-     if(err){console.log("We have an error:");
-             console.log(err);}
-     res.send(rows);
-  }})(res));
+    var sql = 'SELECT * FROM test.till_buttons';
+    connection.query(sql,(function(res){return function(err,rows,fields){
+        if(err){console.log("We have an error:");
+            console.log(err);}
+        res.send(rows);
+    }})(res));
 });
 app.get("/click",function(req,res){
-  var id = req.param('id');
-  var sql = 'YOUR SQL HERE'
-  console.log("Attempting sql ->"+sql+"<-");
+    var id = req.param('id');
+    var sql = 'YOUR SQL HERE'
+    console.log("Attempting sql ->"+sql+"<-");
 
-  connection.query(sql,(function(res){return function(err,rows,fields){
-     if(err){console.log("We have an insertion error:");
-             console.log(err);}
-     res.send(err); // Let the upstream guy know how it went
-  }})(res));
+    connection.query(sql,(function(res){return function(err,rows,fields){
+        if(err){console.log("We have an insertion error:");
+            console.log(err);}
+        res.send(err); // Let the upstream guy know how it went
+    }})(res));
 });
-// Your other API handlers go here!
 
 app.listen(port);
